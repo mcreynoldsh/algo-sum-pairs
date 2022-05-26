2171cb5a958024dac40704492743b3960c4b0654
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-05-26 17:03:11 -0500

    initial py solution

--- python/sum_pairs.py
+++ python/sum_pairs.py
@@ -1,1 +1,14 @@
-def sum_pairs():
+def sum_pairs(num_list, check_sum):
+    pairs_list=[]
+    for i, k in enumerate (num_list):
+        check_list= num_list[i+1:]
+        for n in check_list:
+            if k + n == check_sum:
+                pairs_list.append([k,n])
+    if len(pairs_list) != 0:
+        return pairs_list
+    else:
+        return  'unable to find pairs'            
+
+
+
--- python/sum_pairs_spec.py
+++ python/sum_pairs_spec.py
@@ -1,3 +1,6 @@
 from sum_pairs import sum_pairs
 
+print(sum_pairs([1,3,8,9,4],15)=='unable to find pairs')
+print(sum_pairs([5,3,9,12,56,8,15],20)==[[5,15],[12,8]])
+print(sum_pairs([1,2,3,4,5], 7)==[[2,5],[3,4]])
 # write your specs here!
